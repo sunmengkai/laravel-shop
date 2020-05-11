071580c19491a8b1dc730f6275fc3ac469fb0be8
knight <smk19930219@163.com>
2020-05-11 17:50:49 +0800

    基础布局

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -1,24 +1,25 @@
-<nav class="navbar navbar-expand-lg navbar-light ba-light navbar-static-top">
-<div class="container">
-    <a class="navbar-brand" href="{{ url('/') }}">Laravel - Shop</a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse"
-    data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
-    aria-expanded="false" aria-label="Toggle navigation"
-    >
+<nav class="navbar navbar-expand-lg navbar-light bg-light navbar-static-top">
+    <div class="container">
+      <!-- Branding Image -->
+      <a class="navbar-brand " href="{{ url('/') }}">
+        Laravel Shop
+      </a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collepse navbar-collepse" id="navbarSupportedContent">
+      </button>
+  
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <!-- Left Side Of Navbar -->
         <ul class="navbar-nav mr-auto">
-
+  
         </ul>
+  
+        <!-- Right Side Of Navbar -->
         <ul class="navbar-nav navbar-right">
-            <li class="nav-item">
-                <a class="nav-link" href="#">登录</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">注册</a>
-            </li>
+          <!-- Authentication Links -->
+          <li class="nav-item"><a class="nav-link" href="#">登录</a></li>
+          <li class="nav-item"><a class="nav-link" href="#">注册</a></li>
         </ul>
+      </div>
     </div>
-</div>
-</nav>
+  </nav>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     
     <title>@yield('title','Laravel Shop')-Laravel 电商教程</title>
-    <link herf = "{{ mix('css/app.css') }}" rel="stylesheet" >
+    <link href = "{{ mix('css/app.css') }}" rel="stylesheet" >
 </head>
 <body>
     <div id="app" class="{{ route_class() }}-page">
@@ -17,6 +17,6 @@
         </div>
         @include('layouts._footer')
     </div>
-    <script src="{{mix('js/app.js')}}"></script>
+    <script src="{{ mix('js/app.js') }}"></script>
 </body>
 </html>
