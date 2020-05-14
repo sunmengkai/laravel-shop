3848c6eb0a510a42af2668d9d53668eeaeeb197e
knight <smk19930219@163.com>
2020-05-14 12:59:34 +0800

    邮箱验证

--- routes/web.php
+++ routes/web.php
@@ -13,8 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+//Route::get('/', 'PagesController@root')->name('root')->middleware('verified');
 Route::get('/', 'PagesController@root')->name('root');
-
-Auth::routes();
+Auth::routes(['verify' => true]);
 
 //Route::get('/home', 'HomeController@index')->name('home');
