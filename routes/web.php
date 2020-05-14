1769783506d56a0e9c57ed0723a1a3e2928fc8ce
knight <smk19930219@163.com>
2020-05-14 13:40:15 +0800

    添加收货地址列表

--- routes/web.php
+++ routes/web.php
@@ -16,5 +16,8 @@ use Illuminate\Support\Facades\Route;
 //Route::get('/', 'PagesController@root')->name('root')->middleware('verified');
 Route::get('/', 'PagesController@root')->name('root');
 Auth::routes(['verify' => true]);
+Route::group(['middleware'=>['auth','verified']],function(){
+    Route::get('user_addresses','UserAddressesController@index')->name('user_addresses.index');
+});
 
 //Route::get('/home', 'HomeController@index')->name('home');
